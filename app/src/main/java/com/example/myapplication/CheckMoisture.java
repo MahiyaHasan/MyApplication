232f3b1f7d76e2908bbaea1c1edb863d5f5e0f0c
MahiyaHasan <mahiya.hasan.03@gmail.com>
2020-05-29 17:41:57 +0600

    Mahiya 29 May

--- app/src/main/java/com/example/myapplication/CheckMoisture.java
+++ app/src/main/java/com/example/myapplication/CheckMoisture.java
@@ -1,31 +1,65 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CheckMoisture extends AppCompatActivity {
-Button waterPlant_bt;
+    TextView moisture_tv;
+    Button save_bt;
+    DatabaseReference reff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_moisture);
-        waterPlant_bt = findViewById(R.id.waterPlant_bt);
+        getSupportActionBar().hide();
 
-        waterPlant_bt.setOnClickListener(new View.OnClickListener() {
+        moisture_tv = findViewById(R.id.moisture_et);
 
+
+        // save_bt.setOnClickListener(new View.OnClickListener() {
+
+        // @Override
+        //   public void onClick(View v) {
+        //   reff = FirebaseDatabase.getInstance().getReference().child("Another").child("TestData");
+        reff = FirebaseDatabase.getInstance().getReference().child("Student");
+
+        reff.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Watering.class);
-                startActivity(intent);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren())
+                {
+                    String name = ds.getValue().toString();
+
+                    //String name = ds.child("name").getValue().toString();
+                    //String age = ds.child("age").getValue().toString();
+                    //String phone = ds.child("ph").getValue().toString();
+
+                    moisture_tv.setText(name);
+                    //  b.setText(age);
+                    // c.setText(phone);
+
+                }
+
             }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         });
-
-
+        //   }
+        // });
     }
 }
