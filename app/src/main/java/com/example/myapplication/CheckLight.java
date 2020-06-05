a491ee514e3ab4c5002ee93c4007a5dd499ad2b4
MahiyaHasan <mahiya.hasan.03@gmail.com>
2020-06-05 13:58:21 +0600

    5 june Mahiya buttons replaced by switches

--- app/src/main/java/com/example/myapplication/CheckLight.java
+++ app/src/main/java/com/example/myapplication/CheckLight.java
@@ -11,6 +11,7 @@ import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,7 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class CheckLight extends AppCompatActivity {
-Button startReading_bt,stopReading_bt,turnFlaslightsOn_bt;
+Switch reading_switch, light_switch;
 TextView light_tv;
 DatabaseReference TurnLightOn;
 DatabaseReference CurrentLightIntensity;
@@ -32,105 +33,109 @@ DatabaseReference reff;
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_light);
-        turnFlaslightsOn_bt = findViewById(R.id.turnFlaslightsOn_bt);
-        startReading_bt = findViewById(R.id.startReading_bt);
-        stopReading_bt = findViewById(R.id.stopReading_bt);
+        reading_switch = findViewById(R.id.reading_switch);
+        light_switch = findViewById(R.id.light_switch);
         light_tv = findViewById(R.id.light_tv);
         CurrentLightIntensity = FirebaseDatabase.getInstance().getReference().child("CurrentLightIntensityValue");
 
         reff = FirebaseDatabase.getInstance().getReference();
 
-        startReading_bt.setOnClickListener(new View.OnClickListener() {
-            ///////////////////////////###################
-            @Override
-            public void onClick(View v) {
-                reff.child("SendLightIntensityValue").setValue("1");
+        reading_switch.setOnClickListener(new View.OnClickListener() {
+                                              ///////////////////////////###################
+                                              @Override
+                                              public void onClick(View v) {
+                                                  Boolean switchState = reading_switch.isChecked();
+                                                  if(switchState == true)
+                                                  {
+                                                  reff.child("SendLightIntensityValue").setValue("1");
 
-                CurrentLightIntensity.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                            String moistureValue = ds.getValue().toString().trim();
-                            light_tv.setText(moistureValue);
+                                                  CurrentLightIntensity.addValueEventListener(new ValueEventListener() {
+                                                      @Override
+                                                      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                          for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                                              String moistureValue = ds.getValue().toString().trim();
+                                                              light_tv.setText(moistureValue);
 
 //  int moistureValueInt = Integer.valueOf(moistureValue);
 //  moisture_tv.setText(moistureValueInt);
-                        NotificationCompat.Builder mbuilder = (NotificationCompat.Builder)
-                                new NotificationCompat.Builder(getApplicationContext())
-                                        .setSmallIcon(R.drawable.ic_notification)
-                                        .setContentTitle("Notification")
-                                        //.setSound(defaultSoundUri)
-                                        .setContentText("Soil moisture level is not optimum.");
-                        NotificationManager notificationManager = (NotificationManager)
-                                getSystemService(NOTIFICATION_SERVICE);
-                        if (moistureValue.equals("41")) {
-                            ///////////############ NOTIFICATION ###############//////////////////////
-                            if (notificationFlag2 == 0)
-                            {
-                                notificationManager.notify(0, mbuilder.build());
+                                                              NotificationCompat.Builder mbuilder = (NotificationCompat.Builder)
+                                                                      new NotificationCompat.Builder(getApplicationContext())
+                                                                              .setSmallIcon(R.drawable.ic_notification)
+                                                                              .setContentTitle("Notification")
+                                                                              //.setSound(defaultSoundUri)
+                                                                              .setContentText("Soil moisture level is not optimum.");
+                                                              NotificationManager notificationManager = (NotificationManager)
+                                                                      getSystemService(NOTIFICATION_SERVICE);
+                                                              if (moistureValue.equals("41")) {
+                                                                  ///////////############ NOTIFICATION ###############//////////////////////
+                                                                  if (notificationFlag2 == 0) {
+                                                                      notificationManager.notify(0, mbuilder.build());
 
-                                notificationFlag2++;
+                                                                      notificationFlag2++;
 
-                                ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
-                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
-                            }
+                                                                      ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
+                                                                      toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
+                                                                  }
 
-                            //1  if (notificationFlag == 0) {
+                                                                  //1  if (notificationFlag == 0) {
 
-                            //1   ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
-                            //1 toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
-                            //1 }
-                            //1 notificationFlag++;
+                                                                  //1   ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
+                                                                  //1 toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
+                                                                  //1 }
+                                                                  //1 notificationFlag++;
 
-                            ///////////############ NOTIFICATION ###############//////////////////////
+                                                                  ///////////############ NOTIFICATION ###############//////////////////////
 
-                        }
-                        // else
-                            //  {
-                            // notificationManager.cancel(0);
-                            //
-                            //notificationFlag = 0;
-                            //notificationFlag2 = 0;
-                            //}
-
-
-                        }
+                                                              }
+                                                              // else
+                                                              //  {
+                                                              // notificationManager.cancel(0);
+                                                              //
+                                                              //notificationFlag = 0;
+                                                              //notificationFlag2 = 0;
+                                                              //}
 
 
-                    }
+                                                          }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                    }
+                                                      }
 
-                });
-            }
+                                                      @Override
+                                                      public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                                      }
+
+                                                  });
+                                              }//////////if ends
+
+                                                  else
+                                                  {
+                                                      reff = FirebaseDatabase.getInstance().getReference();
+
+                                                      reff.child("SendLightIntensityValue").setValue("0");
+                                                  }
+                                          }//// on click ends
             //////////////////////###############
         });
 
-        turnFlaslightsOn_bt.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                reff.child("TurnLightOn").setValue("1");
-
-                Intent intent = new Intent(getApplicationContext(), SetTimer.class);
-                startActivity(intent);
-            }
-
-        });
-
-
-        stopReading_bt.setOnClickListener(new View.OnClickListener() {
+        light_switch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Boolean switchState = light_switch.isChecked();
                 reff = FirebaseDatabase.getInstance().getReference();
 
-                reff.child("SendLightIntensityValue").setValue("0");
+                if (switchState == true) {
+                    reff.child("TurnLightOn").setValue("1");
 
+                } else {
+
+                    reff.child("TurnLightOn").setValue("0");
+                }
 
             }
         });
+
     }
+
 }
--- app/src/main/java/com/example/myapplication/CheckMoisture.java
+++ app/src/main/java/com/example/myapplication/CheckMoisture.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,7 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class CheckMoisture extends AppCompatActivity {
     TextView moisture_tv;
-    Button waterPlant_bt,start_bt,stop_bt;
+   Switch reading_switch,pump_switch;
     DatabaseReference reff;
     DatabaseReference sendMoistureSensorValueRef;
     DatabaseReference CurrentMoistureValue;
@@ -37,109 +38,111 @@ public class CheckMoisture extends AppCompatActivity {
         getSupportActionBar().hide();
 
         moisture_tv = findViewById(R.id.moisture_et);
-        waterPlant_bt = findViewById(R.id.waterPlant_bt);
-        start_bt = findViewById(R.id.start_bt);
-        stop_bt = findViewById(R.id.stop_bt);
-
+        reading_switch = findViewById(R.id.reading_switch);
+        pump_switch = findViewById(R.id.pump_switch);
 
         //reff = FirebaseDatabase.getInstance().getReference().child("Another").child("TestData");
         CurrentMoistureValue = FirebaseDatabase.getInstance().getReference().child("CurrentMoistureValue");
         reff = FirebaseDatabase.getInstance().getReference();
         sendMoistureSensorValueRef = FirebaseDatabase.getInstance().getReference().child("SendMoistureSensorData");
-        start_bt.setOnClickListener(new View.OnClickListener() {
+        reading_switch.setOnClickListener(new View.OnClickListener() {
 
             ///////////////////////////###################
             @Override
             public void onClick(View v) {
-                sendMoistureSensorValueRef.setValue("1");
-                CurrentMoistureValue.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Boolean switchState = reading_switch.isChecked();
+                if (switchState == true) {
+                    sendMoistureSensorValueRef.setValue("1");
+                    CurrentMoistureValue.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                            String moistureValue = ds.getValue().toString().trim();
+                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                String moistureValue = ds.getValue().toString().trim();
 
-                            moisture_tv.setText(moistureValue);
+                                moisture_tv.setText(moistureValue);
 
 //                    int moistureValueInt = Integer.valueOf(moistureValue);
 
-                            //  moisture_tv.setText(moistureValueInt);
+                                //  moisture_tv.setText(moistureValueInt);
 
-                            NotificationCompat.Builder mbuilder = (NotificationCompat.Builder)
-                                    new NotificationCompat.Builder(getApplicationContext())
-                                            .setSmallIcon(R.drawable.ic_notification)
-                                            .setContentTitle("Notification")
-                                            //.setSound(defaultSoundUri)
-                                            .setContentText("Soil moisture level is not optimum.");
-                            NotificationManager notificationManager = (NotificationManager)
-                                    getSystemService(NOTIFICATION_SERVICE);
-                            if (moistureValue.equals("41")) {
-                                ///////////############ NOTIFICATION ###############//////////////////////
-                                if (notificationFlag2 == 0)
-                                {
-                                    notificationManager.notify(0, mbuilder.build());
+                                NotificationCompat.Builder mbuilder = (NotificationCompat.Builder)
+                                        new NotificationCompat.Builder(getApplicationContext())
+                                                .setSmallIcon(R.drawable.ic_notification)
+                                                .setContentTitle("Notification")
+                                                //.setSound(defaultSoundUri)
+                                                .setContentText("Soil moisture level is not optimum.");
+                                NotificationManager notificationManager = (NotificationManager)
+                                        getSystemService(NOTIFICATION_SERVICE);
+                                if (moistureValue.equals("41")) {
+                                    ///////////############ NOTIFICATION ###############//////////////////////
+                                    if (notificationFlag2 == 0) {
+                                        notificationManager.notify(0, mbuilder.build());
 
-                                notificationFlag2++;
+                                        notificationFlag2++;
 
-                                ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
-                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
+                                        ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
+                                        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
+                                    }
+
+                                    //1  if (notificationFlag == 0) {
+
+                                    //1   ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
+                                    //1 toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
+                                    //1 }
+                                    //1 notificationFlag++;
+
+                                    ///////////############ NOTIFICATION ###############//////////////////////
+
+                                }
+                                // else
+                                //  {
+                                // notificationManager.cancel(0);
+                                //
+                                //notificationFlag = 0;
+                                //notificationFlag2 = 0;
+                                //}
+
+
                             }
-
-                              //1  if (notificationFlag == 0) {
-
-                                 //1   ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 1000);
-                                   //1 toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 6000);
-                               //1 }
-                                //1 notificationFlag++;
-
-                                ///////////############ NOTIFICATION ###############//////////////////////
-
-                            }
-                           // else
-                          //  {
-                           // notificationManager.cancel(0);
-                            //
-                            //notificationFlag = 0;
-                            //notificationFlag2 = 0;
-                            //}
 
 
                         }
 
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                    }
+                        }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    });
+                }// end of in checked switch
+                else {
+                    reff = FirebaseDatabase.getInstance().getReference();
 
-                    }
-
-                });
+                    reff.child("SendMoistureSensorData").setValue("0");
+                }
             }
             //////////////////////###############
         });
 
-         waterPlant_bt.setOnClickListener(new View.OnClickListener() {
-          @Override
-         public void onClick(View v) {
-           reff = FirebaseDatabase.getInstance().getReference();
-
-                   reff.child("TurnPumpOn").setValue("1");
-
-
-               }
-          });
-
-        stop_bt.setOnClickListener(new View.OnClickListener() {
+        pump_switch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Boolean switchState = pump_switch.isChecked();
                 reff = FirebaseDatabase.getInstance().getReference();
 
-                reff.child("SendMoistureSensorData").setValue("0");
+                if (switchState == true) {
+                    reff.child("TurnPumpOn").setValue("1");
 
+                } else {
+
+                    reff.child("TurnPumpOn").setValue("0");
+                }
 
             }
         });
 
     }
-}
+
+    }
+
